aa084c993623c4651a316e40b7e4b30ff95ac2e7
Evandro dos Santos Dias <vando86206@gmail.com>
2025-08-25 09:52:15 -0300

    instalação de dependencias npm i nodemon express mysql2 cors bcrypt

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+// import express from 'express';
+// import cors from 'cors'
+// import dotenv from 'dotenv'
+// import authRoutes from './routes/authRoutes.js'
+// dotenv.config()
+
+
+// const app = express()
+// app.use(cors({
+//     origin: '*', // Domínio do Angular http://localhost:4200
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+// app.use(express.json())
+// app.use('/api/auth', authRoutes)
+
+
+// const PORT = process.env.PORT || 3000
+// app.listen(PORT, () => {
+//     console.log(`Servidor rodando: http://localhost:${PORT}`)
+// })'
